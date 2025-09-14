f70abaf15f714230e3f710addf5e8b2ef0fcd95b
Anastasia Makus <asya.makus@yandex.com>
2025-09-14 17:05:38 +0200

    HW-14 using By.cssSelector and By.xPath strategies.

--- src/test/java/com/ait/qa65/FirstSeleniumTest.java
+++ src/test/java/com/ait/qa65/FirstSeleniumTest.java
@@ -1,5 +1,6 @@
 package com.ait.qa65;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
