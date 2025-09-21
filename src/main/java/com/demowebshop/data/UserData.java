4137e267f936e6106e03a42b625eff6bf73a01a7
Anastasia Makus <asya.makus@yandex.com>
2025-09-21 20:00:37 +0200

    HW-19 fix password

--- src/main/java/com/demowebshop/data/UserData.java
+++ src/main/java/com/demowebshop/data/UserData.java
@@ -4,5 +4,5 @@ public class UserData {
     public static final String firstNAME ="John";
     public static final String lastNAME ="Doe";
     public static final String EMAIL ="rotTest@gmail.com";
-    public static final String PASSWORD ="HW123";
+    public static final String PASSWORD ="HW123!";
 }
