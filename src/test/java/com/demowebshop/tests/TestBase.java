1e064aa11f428603b8af793677a83c4456661cff
Anastasia Makus <asya.makus@yandex.com>
2025-09-21 21:39:10 +0200

    HW-19 add logger

--- src/test/java/com/demowebshop/tests/TestBase.java
+++ src/test/java/com/demowebshop/tests/TestBase.java
@@ -2,11 +2,16 @@ package com.demowebshop.tests;
 
 import com.demowebshop.fw.ApplicationManager;
 import org.openqa.selenium.remote.Browser;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeMethod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.testng.ITestResult;
+import org.testng.annotations.*;
 
-public class TestBase{
+import java.lang.reflect.Method;
+
+public class TestBase {
+    Logger logger = LoggerFactory.getLogger(TestBase.class);
+
     protected ApplicationManager app = new ApplicationManager(
             System.getProperty("browser", Browser.CHROME.browserName())
     );
@@ -16,14 +21,31 @@ public class TestBase{
         return app.generateEmail();
     }
 
-    @BeforeMethod
+    @BeforeSuite
     public void setUp() {
         app.init();
     }
 
+    @BeforeMethod
+    public void startTest(Method method) {
+        logger.info("Start test " + method.getName());
+    }
+
     @AfterMethod
+    public void stopTest(ITestResult result) {
+        if (result.isSuccess()) {
+            logger.info("PASSED: " + result.getMethod().getMethodName());
+        } else {
+            logger.error("FAILED: " + result.getMethod()
+                    .getMethodName() + " Screenshot path: " + app.getUser()
+                    .takeScreenshot());
+        }
+        logger.info("Stop test");
+        logger.info("*********************************************************");
+    }
+
+    @AfterSuite
     public void tearDown() {
         app.stop();
     }
-
 }
